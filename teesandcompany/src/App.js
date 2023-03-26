7386c38053027c7e6ae6b61ad04b594341187a88
sourabh-rao <124281538+sourabh-rao@users.noreply.github.com>
2023-03-27 01:05:14 +0530

    navbar

--- teesandcompany/src/App.js
+++ teesandcompany/src/App.js
@@ -2,17 +2,12 @@ import './App.css';
 import Footer from './Footer';
 import navbar from './navbar';
 
-import logo from './TeesLogo.png';
-
 function App() {
   return (
   <div className='sr'>
 
     <div>
       {navbar()},
-      <div className="App-logo">
-        <img src={logo} className="App-logo" alt="logo" />
-    </div> ,
       {Footer()}
     </div>
   </div>
