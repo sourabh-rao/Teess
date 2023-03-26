f5ab559be02fbace5d74825ebe5347c71eef159b
sourabh-rao <124281538+sourabh-rao@users.noreply.github.com>
2023-03-27 01:05:39 +0530

    navbar

--- teesandcompany/src/navbar.js
+++ teesandcompany/src/navbar.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import './navbar.css'
+import logo from './Teeslogo.jpg';
 
 function navbar() {
   return (
     <div id="navbar-animmenu">
-      <ul class="show-dropdown main-navbar">
-        <div class="hori-selector">
-          <div class="left"></div>
-          <div class="right"></div>
-        </div>
-        <li><b>home</b></li>
-        <li>collection</li>
-        <li>Components</li>
-      </ul>
+        <ul class="show-dropdown main-navbar">
+            <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+            <li>
+            {/* { <div className="App-logo"> */}
+               <img src={logo} className="App-logo" alt="logo" />
+                {/* </div>} */}
+            </li>
+            <li>
+                <i class="fas fa-tachometer-alt"></i>Dashboard
+            </li>
+            <li class="active">
+                <i class="far fa-address-book"></i>Address Book
+            </li>
+            <li>
+                <i class="far fa-clone"></i>Components
+            </li>
+        </ul>
     </div>
   );
 }
