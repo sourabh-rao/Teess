f289360e595caca6d40449ba51ae95927e23eabe
Vishal-Brilliance <vishal.brilliance@gmail.com>
2023-03-27 02:59:19 +0530

    prod

--- teesandcompany/src/App.js
+++ teesandcompany/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Footer from "./Footer";
 import navbar from "./Navbar";
+import Products from "./Products";
 
 function App() {
   return (
     <div className="sr">
       <div>
         {navbar()}
-        <div className="space"></div>
+        {Products()}
         {Footer()}
       </div>
     </div>
