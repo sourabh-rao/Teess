ffc73cb259c4ebb85b3c60fcf321d685eea86c7d
Vishal-Brilliance <vishal.brilliance@gmail.com>
2023-03-27 04:30:52 +0530

    prod home

--- teesandcompany/src/Products.js
+++ teesandcompany/src/Products.js
@@ -1,253 +1,245 @@
 import React from "react";
 import "./Products.css";
+import demo1 from "./demoimages/demo1.jpg";
+import demo2 from "./demoimages/demo2.jpg";
+import demo3 from "./demoimages/demo3.jpg";
+import demo4 from "./demoimages/demo4.jpg";
+import demo5 from "./demoimages/demo5.jpg";
+import demo6 from "./demoimages/demo6.jpg";
+import demo7 from "./demoimages/demo7.jpg";
+import demo8 from "./demoimages/demo8.jpg";
 
 function Products() {
   return (
     <div>
       <div className="first container-fluid bg-trasparent my-4 p-3">
         <div className="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+          
+          
           <div className="col hp">
             <div className="card h-100 shadow-sm">
               <a href="/">
-                <img
-                  src="https://m.media-amazon.com/images/I/81gK08T6tYL._AC_SL1500_.jpg"
-                  className="card-img-top"
-                  alt="product.title"
-                />
+                <img src={demo1} className="card-img-top" alt="product.title" />
               </a>
 
-              <div className="label-top shadow-sm">
-                <a className="text-white" href="/">
-                  asus
-                </a>
-              </div>
+              <div className="label-top shadow-sm">CAP</div>
               <div className="card-body">
                 <div className="clearfix mb-3">
                   <span className="float-start badge rounded-pill bg-success">
-                    1.245$
-                  </span>
-
-                  <span className="float-end">
-                    <a
-                      href="/"
-                      className="small text-muted text-uppercase aff-link"
-                    >
-                      reviews
-                    </a>
+                    PRICE HERE
                   </span>
                 </div>
                 <h5 className="card-title">
                   <a target="_blank" href="/">
-                    ASUS TUF FX505DT Gaming Laptop- 15.6", 120Hz Full HD, AMD
-                    Ryzen 5 R5-3550H Processor, GeForce GTX 1650 Graphics, 8GB
-                    DDR4, 256GB PCIe SSD, RGB Keyboard, Windows 10 64-bit -
-                    FX505DT-AH51
+                    ARMY CAP , Cap With Army Design
                   </a>
                 </h5>
 
                 <div className="d-grid gap-2 my-4">
                   <a href="/" className="btn btn-warning bold-btn">
-                    add to cart
+                    View Product
                   </a>
-                </div>
-                <div className="clearfix mb-1">
-                  <span className="float-start">
-                    <a href="/">
-                      <i className="fas fa-question-circle"></i>
-                    </a>
-                  </span>
-
-                  <span className="float-end">
-                    <i
-                      className="far fa-heart"
-                      StyleHTMLAttributes="cursor: pointer"
-                    ></i>
-                  </span>
                 </div>
               </div>
             </div>
           </div>
+
           <div className="col hp">
             <div className="card h-100 shadow-sm">
               <a href="/">
-                <img
-                  src="https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_SL1500_.jpg"
-                  className="card-img-top"
-                  alt="product.title"
-                />
+                <img src={demo2} className="card-img-top" alt="product.title" />
               </a>
 
-              <div className="label-top shadow-sm">
-                <a className="text-white" href="/">
-                  razer
-                </a>
-              </div>
+              <div className="label-top shadow-sm">Tee Combos</div>
               <div className="card-body">
                 <div className="clearfix mb-3">
                   <span className="float-start badge rounded-pill bg-success">
-                    2.345$
-                  </span>
-
-                  <span className="float-end">
-                    <a
-                      href="/"
-                      className="small text-muted text-uppercase aff-link"
-                    >
-                      reviews
-                    </a>
+                    PRICE HERE
                   </span>
                 </div>
                 <h5 className="card-title">
                   <a target="_blank" href="/">
-                    Razer Blade 15 Base Gaming Laptop 2020: Intel Core i7-10750H
-                    6-Core, NVIDIA GeForce GTX 1660 Ti, 15.6" FHD 1080p 120Hz,
-                    16GB RAM, 256GB SSD, CNC Aluminum, Chroma RGB Lighting,
-                    Black
+                    T-Shirt Combo , 2 Black Tees 
                   </a>
                 </h5>
 
                 <div className="d-grid gap-2 my-4">
                   <a href="/" className="btn btn-warning bold-btn">
-                    add to cart
+                    View Product
                   </a>
-                </div>
-                <div className="clearfix mb-1">
-                  <span className="float-start">
-                    <a href="/">
-                      <i className="fas fa-question-circle"></i>
-                    </a>
-                  </span>
-
-                  <span className="float-end">
-                    <i
-                      className="far fa-heart"
-                      StyleHTMLAttributes="cursor: pointer"
-                    ></i>
-                  </span>
                 </div>
               </div>
             </div>
           </div>
+
           <div className="col hp">
             <div className="card h-100 shadow-sm">
               <a href="/">
-                <img
-                  src="https://m.media-amazon.com/images/I/81w+3k4U8PL._AC_SL1500_.jpg"
-                  className="card-img-top"
-                  alt="product.title"
-                />
+                <img src={demo3} className="card-img-top" alt="product.title" />
               </a>
 
-              <div className="label-top shadow-sm">
-                <a className="text-white" href="/">
-                  lenovo
-                </a>
-              </div>
+              <div className="label-top shadow-sm">COMBO</div>
               <div className="card-body">
                 <div className="clearfix mb-3">
                   <span className="float-start badge rounded-pill bg-success">
-                    1.020$
-                  </span>
-
-                  <span className="float-end">
-                    <a
-                      href="/"
-                      className="small text-muted text-uppercase aff-link"
-                    >
-                      reviews
-                    </a>
+                    PRICE HERE
                   </span>
                 </div>
                 <h5 className="card-title">
                   <a target="_blank" href="/">
-                    Lenovo Legion 5 Gaming Laptop, 15.6" FHD (1920x1080) IPS
-                    Screen, AMD Ryzen 7 4800H Processor, 16GB DDR4, 512GB SSD,
-                    NVIDIA GTX 1660Ti, Windows 10, 82B1000AUS, Phantom Black
+                    Black Tee And Lower Combo
                   </a>
                 </h5>
 
                 <div className="d-grid gap-2 my-4">
                   <a href="/" className="btn btn-warning bold-btn">
-                    add to cart
+                    View Product
                   </a>
-                </div>
-                <div className="clearfix mb-1">
-                  <span className="float-start">
-                    <a href="/">
-                      <i className="fas fa-question-circle"></i>
-                    </a>
-                  </span>
-
-                  <span className="float-end">
-                    <i
-                      className="far fa-heart"
-                      StyleHTMLAttributes="cursor: pointer"
-                    ></i>
-                  </span>
                 </div>
               </div>
             </div>
           </div>
+
           <div className="col hp">
             <div className="card h-100 shadow-sm">
               <a href="/">
-                <img
-                  src="https://m.media-amazon.com/images/I/61Ze2wc9nyS._AC_SL1500_.jpg"
-                  className="card-img-top"
-                  alt="product.title"
-                />
+                <img src={demo4} className="card-img-top" alt="product.title" />
               </a>
-              {/* <!-- <div className="label-top shadow-sm">Asus Rog</div>  --> */}
-              <div className="label-top shadow-sm">
-                <a className="text-white" href="/">
-                  msi
-                </a>
-              </div>
+
+              <div className="label-top shadow-sm">COMBO</div>
               <div className="card-body">
                 <div className="clearfix mb-3">
                   <span className="float-start badge rounded-pill bg-success">
-                    2.245$
-                  </span>
-
-                  <span className="float-end">
-                    <a
-                      href="/"
-                      className="small text-muted text-uppercase aff-link"
-                    >
-                      reviews
-                    </a>
+                    PRICE HERE
                   </span>
                 </div>
                 <h5 className="card-title">
                   <a target="_blank" href="/">
-                    MSI GL66 Gaming Laptop: 15.6" 144Hz FHD 1080p Display, Intel
-                    Core i7-11800H, NVIDIA GeForce RTX 3070, 16GB, 512GB SSD,
-                    Win10, Black (11UGK-001)
+                  Blue Tee And Lower Combo
                   </a>
                 </h5>
 
                 <div className="d-grid gap-2 my-4">
                   <a href="/" className="btn btn-warning bold-btn">
-                    add to cart
+                    View Product
                   </a>
-                </div>
-                <div className="clearfix mb-1">
-                  <span className="float-start">
-                    <a href="/">
-                      <i className="fas fa-question-circle"></i>
-                    </a>
-                  </span>
-
-                  <span className="float-end">
-                    <i
-                      className="far fa-heart"
-                      StyleHTMLAttributes="cursor: pointer"
-                    ></i>
-                  </span>
                 </div>
               </div>
             </div>
           </div>
+
+          <div className="col hp">
+            <div className="card h-100 shadow-sm">
+              <a href="/">
+                <img src={demo5} className="card-img-top" alt="product.title" />
+              </a>
+
+              <div className="label-top shadow-sm">SHIRT</div>
+              <div className="card-body">
+                <div className="clearfix mb-3">
+                  <span className="float-start badge rounded-pill bg-success">
+                    PRICE HERE
+                  </span>
+                </div>
+                <h5 className="card-title">
+                  <a target="_blank" href="/">
+                    Two Shirt Combo
+                  </a>
+                </h5>
+
+                <div className="d-grid gap-2 my-4">
+                  <a href="/" className="btn btn-warning bold-btn">
+                    View Product
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col hp">
+            <div className="card h-100 shadow-sm">
+              <a href="/">
+                <img src={demo6} className="card-img-top" alt="product.title" />
+              </a>
+
+              <div className="label-top shadow-sm">LOWER</div>
+              <div className="card-body">
+                <div className="clearfix mb-3">
+                  <span className="float-start badge rounded-pill bg-success">
+                    PRICE HERE
+                  </span>
+                </div>
+                <h5 className="card-title">
+                  <a target="_blank" href="/">
+                    ARMY LOWER , Lower With Army Effects
+                  </a>
+                </h5>
+
+                <div className="d-grid gap-2 my-4">
+                  <a href="/" className="btn btn-warning bold-btn">
+                    View Product
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col hp">
+            <div className="card h-100 shadow-sm">
+              <a href="/">
+                <img src={demo7} className="card-img-top" alt="product.title" />
+              </a>
+
+              <div className="label-top shadow-sm">TEES</div>
+              <div className="card-body">
+                <div className="clearfix mb-3">
+                  <span className="float-start badge rounded-pill bg-success">
+                    PRICE HERE
+                  </span>
+                </div>
+                <h5 className="card-title">
+                  <a target="_blank" href="/">
+                    Pair of Two White Shirts
+                  </a>
+                </h5>
+
+                <div className="d-grid gap-2 my-4">
+                  <a href="/" className="btn btn-warning bold-btn">
+                    View Product
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col hp">
+            <div className="card h-100 shadow-sm">
+              <a href="/">
+                <img src={demo8} className="card-img-top" alt="product.title" />
+              </a>
+
+              <div className="label-top shadow-sm">JACKETS</div>
+              <div className="card-body">
+                <div className="clearfix mb-3">
+                  <span className="float-start badge rounded-pill bg-success">
+                    PRICE HERE
+                  </span>
+                </div>
+                <h5 className="card-title">
+                  <a target="_blank" href="/">
+                    Green Jackets
+                  </a>
+                </h5>
+
+                <div className="d-grid gap-2 my-4">
+                  <a href="/" className="btn btn-warning bold-btn">
+                    View Product
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
@@ -255,3 +247,64 @@ function Products() {
 }
 
 export default Products;
+
+// {/* Heree */}
+// <div className="col hp">
+//   <div className="card h-100 shadow-sm">
+//     <a href="/">
+//       <img src={demo1} className="card-img-top" alt="product.title" />
+//     </a>
+
+//     <div className="label-top shadow-sm">
+//       {/* <a className="text-white" href="/">
+//         CAP
+//       </a> */}
+//       CAP
+//     </div>
+//     <div className="card-body">
+//       <div className="clearfix mb-3">
+//         <span className="float-start badge rounded-pill bg-success">
+//           PRICE HERE
+//         </span>
+
+//         {/* <span className="float-end">
+//           <a
+//             href="/"
+//             className="small text-muted text-uppercase aff-link"
+//           >
+//             reviews
+//           </a>
+//         </span> */}
+
+//       </div>
+//       <h5 className="card-title">
+//         <a target="_blank" href="/">
+//           ARMY CAP , Description Here
+//         </a>
+//       </h5>
+
+//       <div className="d-grid gap-2 my-4">
+//         <a href="/" className="btn btn-warning bold-btn">
+//           View Product
+//         </a>
+//       </div>
+
+//       {/* <div className="clearfix mb-1">
+//         <span className="float-start">
+//           <a href="/">
+//             <i className="fas fa-question-circle"></i>
+//           </a>
+//         </span>
+
+//         <span className="float-end">
+//           <i
+//             className="far fa-heart"
+//             StyleHTMLAttributes="cursor: pointer"
+//           ></i>
+//         </span>
+//       </div> */}
+//     </div>
+//   </div>
+// </div>
+
+// {/* till Here */}
