2269a8c83c3190cd86b63275f62c6a9244438ba8
sourabh-rao <124281538+sourabh-rao@users.noreply.github.com>
2023-03-26 23:37:12 +0530

    navbar pic

--- teesandcompany/src/App.js
+++ teesandcompany/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-
 import navbar from './navbar';
+
+import logo from './TeesLogo.png';
 
 function App() {
   return (
-  
-  <div>
-{navbar()}
+  <div className='sr'>
+    <div className="App-logo">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+    <div>
+      {navbar()}
+    </div>
   </div>
+  
+ 
+
   );
 }
 
