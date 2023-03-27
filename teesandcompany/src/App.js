3d74f33c821d6caecd35c8a5939361237b9c1f2b
sourabh-rao <124281538+sourabh-rao@users.noreply.github.com>
2023-03-27 06:42:34 +0530

    src

--- teesandcompany/src/App.js
+++ teesandcompany/src/App.js
@@ -2,14 +2,12 @@ import "./App.css";
 import Footer from "./Footer";
 import navbar from "./Navbar";
 import Products from "./Products";
-import Slider from "./Slider";
 
 function App() {
   return (
     <div className="sr">
       <div>
         {navbar()}
-        {Slider()}
         {Products()}
         {Footer()}
       </div>
